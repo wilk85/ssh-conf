1257a84325da34d56a8ef3b40be8d9f1ea0f6b53
wilk85 <wilk85@gmail.com>
2024-02-18 10:18:38 +0100

    script update

--- ssh-server.sh
+++ ssh-server.sh
@@ -2,22 +2,27 @@
 # start script on system to setup ssh server
 
 function install_ssh {
+    echo " + Install openssh packages"
     sudo apt install openssh-client openssh-server -y
 }
 
-function sshd_backup {
-    sudo cp /etc/ssh/sshd_config ~/sshd_config.bkp
-    sudo chmod a-w ~/sshd_config.bkp
+function sshd_config {
+    echo " + Configure sshd" 
+    sudo cp -v /etc/ssh/sshd_config ${HOME}/sshd_config.bkp
+    sudo cp -v ${PWD}/sshd_config.new /etc/ssh/sshd_config
+    sudo chmod a-w ${HOME}/sshd_config.bkp
 }
 
 function setup_remote_user {
-    remoteUser='remote'
+    echo " + Setup remote user"
+    local remoteUser='remote'
     sudo useradd -m -s /bin/bash "$remoteUser"
     sudo usermod -aG sudo "$remoteUser"
 }
 
-function firewall {
-    ufwStatus=$(sudo ufw status | head -n 1 | cut -d ':' -f2 | tr -d " ")
+function firewall_config {
+    echo " + Configure firewall for ssh"
+    local ufwStatus=$(sudo ufw status | head -n 1 | cut -d ':' -f2 | tr -d " ")
     if [[ "$ufwStatus" == "nieaktywny" || "$ufwStatus" == "notactive" ]]; then
         sudo ufw enable
     fi
@@ -25,20 +30,16 @@ function firewall {
     sudo ufw allow 1022/tcp
 }
 
-function copy_sshd_config {
-    sudo cp sshd_config.new /etc/ssh/sshd_config
-}
-
 function start_ssh_svc {
+    echo " + Start ssh service"
     sudo systemctl enable --now ssh
 }
 
 
 install_ssh
-sshd_backup
+sshd_config
 setup_remote_user
-copy_sshd_config
-firewall
+firewall_config
 start_ssh_svc
 
 
